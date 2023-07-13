6724054fe0acce5f211965faaddfa402db60f6d4
Ayushi <116718528+Ayushinitp@users.noreply.github.com>
2023-07-13 18:40:30 +0530

    Add files via upload

---
+++ c7.c
@@ -0,0 +1,119 @@
+//1
+// #include <stdio.h>
+// int main(){
+//     int i;
+//     for(i=1;i<=5;i++)
+//     printf("NIT Patna\n");
+//     return 0;
+// }
+
+
+
+
+//2
+// #include <stdio.h>
+//  int main(){
+//     int i;
+//     for(i=1;i<=10;i++)
+//     printf("%d\t",i);
+//     return 0;
+//  }
+
+
+
+
+
+//3
+// #include <stdio.h>
+//  int main(){
+//     int i;
+//     for(i=10;i>=1;i--)
+//     printf("%d\t",i);
+//     return 0;
+//  }
+
+
+
+
+
+//4
+// #include <stdio.h>
+//  int main(){
+//     int i;
+//     for(i=1;i<20;i++){
+//     if(i%2)
+//     printf("%d\t",i);
+//  }
+//  return 0;
+//  }
+
+
+
+//5
+// #include <stdio.h>
+//  int main(){
+//     int i;
+//     for(i=20;i>0;i--){
+//     if(i%2)
+//     printf("%d\t",i);
+//     }
+//     return 0;
+//  }
+
+
+
+//6
+// #include <stdio.h>
+//  int main(){
+//     int i;
+//     for(i=2;i<=20;i++){
+//         if(i%2==0)
+//         printf("%d\t",i);
+//     }
+//     return 0;
+//  }
+
+
+
+
+//7
+// #include <stdio.h>
+//  int main(){
+//      int i;
+//      for(i=20;i>=2;i--){
+//         if(i%2==0)
+//         printf("%d\t",i);
+//      }
+//      return 0;
+//  }
+
+
+
+
+//8 , 9
+// #include <stdio.h>
+//  int main(){
+//      int i;
+//      printf("Square root of first 10 natural numbers are : \n");
+//     for(i=1;i<=10;i++){
+//         printf("%d\t",i*i);
+//     }
+//      printf("\nCube root of first 10 natural numbers are : \n");
+//     for(i=1;i<=10;i++){
+//         printf("%d\t",i*i*i);
+//     }
+//     return 0;
+//  }
+
+
+
+
+
+//10
+// #include <stdio.h>
+// int main(){
+//     int i;
+//     for(i=1;i<=10;i++)
+//         printf("5*%d=%d\n",i,5*i);
+//     return 0;
+// }
